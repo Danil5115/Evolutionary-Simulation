7652dbc9cfae3c086f8065e541623d1c7ac8ce37
Danil Korchagin <120101246+Danil5115@users.noreply.github.com>
2023-10-29 21:49:56 +0100

    Update comm.py

--- Mountain climbing algorithm.py
+++ Mountain climbing algorithm.py
@@ -4,7 +4,7 @@ M = 30 # backpack capacity
     #kg, cost
 a = [(3, 150), (4, 250), (2, 100), (6, 300), (1, 50), (5, 200), (3, 150), (4, 250), (2, 100), (6, 300), (1, 50), (5, 200), (3, 150), (4, 250), (2, 100), (6, 300), (1, 50), (5, 200), (3, 150), (4, 250), (2, 100), (6, 300), (1, 50), (5, 200), (3, 150), (4, 250), (2, 100), (6, 300), (1, 50), (5, 200)]
 
-#Считаем стоимость рюкзака и его вес Bereme v úvahu náklady na batoh a jeho váhu
+#We calculate the cost of the backpack and its weight Bereme v úvahu náklady na batoh a jeho váhu
 def backpack_value(alpha, a, M):
     cost, weight = 0, 0
     for i, (w, c) in enumerate(a):
@@ -17,11 +17,11 @@ def backpack_value(alpha, a, M):
                 weight += w
     return cost
 
-#Рандомно составляем бинарный список предметов в рюкзаке Náhodně sestavte binární seznam položek v batohu
+#Randomly compose a binary list of items in the backpack Náhodně sestavte binární seznam položek v batohu
 def random_alpha(size):
     return "".join([str(random.randint(0, 1)) for i in range(size)])
 
-#Мутируем члены из данного рюкзака Zmutujte členy z daného batohu
+#Mutate members from this backpack Zmutujte členy z daného batohu
 def mutation(alfa, pmut):
     return "".join(str(1 + int(alfa[i]) * -1) if random.random() < pmut else alfa[i] for i in range(len(alfa)))
 
@@ -30,8 +30,8 @@ def hill_climb(f, tmax, k, c0, pmut):
     alpha_max = None
     alpha = random_alpha(k)
     for attempt in range(tmax):
-        U = [mutation(alpha, pmut) for i in range(c0)]  #Создаёт лист из 10 вариантов мутаций
-        alpha = max(U, key=lambda alpha: f(alpha)) #из списка 10 мы ищем лучший вариант, key лямбда функция которая принимает аргумент и возвращает значени функции f
+        U = [mutation(alpha, pmut) for i in range(c0)]  #Creates a list of 10 mutation options
+        alpha = max(U, key=lambda alpha: f(alpha)) 
         f_x = f(alpha)
         if f_x > f_max:
             f_max =  f_x
